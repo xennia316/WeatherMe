b0e057a209cbb6e8b67cc50cb31258e8a3eb3cbc
xennia316 <xennia316@gmail.com>
2023-06-26 12:32:19 +0100

    consuming api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+	const [search, setSearch] = useState("");
+	const [weather, setWeather] = useState("");
+
 	const api = {
 		key: "067cfe223954844c0c2f03e98431834f",
 		base: "https://api.openweathermap.org/data/2.5/",
 	};
+
+	const searching = () => {
+		fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+			.then((res) => res.json())
+			.then((result) => {
+				setWeather(result);
+				console.log(result);
+			});
+	};
+
 	return (
 		<div className="App">
-			{/* Header goes here */}
-			<h1>Weather Me</h1>
+			<header className="App-header">
+				{/* Header goes here */}
+				<h1>Weather Me</h1>
 
-			{/* Search Box */}
+				{/* Search Box */}
 
-			<input placeholder="Enter your city" type="text" />
+				<div>
+					<input
+						placeholder="Enter your city"
+						type="text"
+						onChange={(e) => {
+							setSearch(e.target.value);
+						}}
+					/>
+					<button onClick={searching}>Search</button>
+				</div>
 
-			{/* Location */}
-
-			<p>Sandpit, Buea</p>
-
-			{/* Temperature */}
-
-			<p>32 degrees celsius</p>
-
-			{/* Weather Condition */}
-
-			<p>Mostly sunny</p>
+				{typeof weather.main != "undefined" ? (
+					<div>
+						<p>{weather.name}</p>
+						<p>{weather.main.temp}</p>
+						<p>{weather.weather[0].main}</p>
+						<p>{weather.weather[0].description}</p>{" "}
+					</div>
+				) : (
+					""
+				)}
+			</header>
 		</div>
 	);
 };
